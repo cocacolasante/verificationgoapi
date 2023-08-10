b6e68785ae116e90fc8887b91c7d3978c65c2bc1
cocacolasante <105400476+cocacolasante@users.noreply.github.com>
2023-08-10 13:56:24 -0400

    manual testing for sc function

--- main/main.go
+++ main/main.go
@@ -3,10 +3,29 @@ package main
 import (
 	"fmt"
 	"verificationapi/client"
+	"verificationapi/contractinterfaces"
+
+	"github.com/ethereum/go-ethereum/common"
 )
 
 func main(){
 	newClient := client.ConnectToClient()
 
 	fmt.Println("Connected to", newClient)
+
+	fee := contractinterfaces.GetFee()
+
+	fmt.Println(fee)
+
+	addy := common.HexToAddress("0x11273F391609BF4C05CA23c6aD29D919a71dc37E")
+
+	postsSlice := contractinterfaces.GetAllUsersPosts(addy)
+
+	fmt.Println(postsSlice)
+
+	// postTwo := contractinterfaces.GetSpecificPost(addy, 2)
+
+	// fmt.Println(postTwo)
+
+	
 }
